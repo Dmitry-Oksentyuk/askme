6f4c75a3eb0ae37af92fec0fa7a9d882d3cc2bf8
Dmitry-Oksentyuk <reklama@gmail.com>
2017-01-09 23:15:50 +0300

    color avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,15 @@ module ApplicationHelper
     end
   end
 
+  def user_color(user)
+    if user.color.present?
+      user.color
+    else
+      user.color = 0
+    end
+  end
+
+
   # Хелпер, рисующий span тэг с иконкой из font-awesome
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
